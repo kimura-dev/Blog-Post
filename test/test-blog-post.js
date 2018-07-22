d5b891b39585d6b0fd6f983188d0be0250427e4a
Scott Reyell <smreyell@gmail.com>
2018-07-22 16:13:56 -0700

    checked solution

--- test/test-blog-post.js
+++ test/test-blog-post.js
@@ -1,15 +1,13 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-
-const {app, runServer, closeServer} = require('../server');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
 const expect = chai.expect;
 
+const { app, runServer, closeServer } = require("../server");
+
 chai.use(chaiHttp);
 
-
-describe('BlogPost', function() {
-
+describe("Blog Posts", function() {
   before(function() {
     return runServer();
   });
@@ -18,49 +16,61 @@ describe('BlogPost', function() {
     return closeServer();
   });
 
-  it('should list blog-posts on GET', function() {
-    return chai.request(app)
-      .get('/blog-posts')
+  it("should list items on GET", function() {
+    return chai
+      .request(app)
+      .get("/blog-posts")
       .then(function(res) {
-
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-
+        expect(res.body).to.be.a("array");
         expect(res.body.length).to.be.above(0);
         res.body.forEach(function(item) {
-          expect(item).to.be.a('object');
-          expect(item).to.have.all.keys('id', 'title', 'content','author','publishedDate');
+          expect(item).to.be.a("object");
+          expect(item).to.have.all.keys(
+            "id",
+            "title",
+            "content",
+            "author",
+            "publishDate"
+          );
         });
       });
   });
 
-  // // id: uuid.v4(),
-  // title: title,
-  // content: content,
-  // author: author,
-  // publishDate: publishDate || Date.now()
-  it('should add a blog-post on POST',
-    function() {
-      const newBlogPost = {
-        title: 'New blog-post', content: 'New blog post content', author:'John Doe'};
+  it("should add a blog post on POST", function() {
+    const newPost = {
+      title: "Lorem ip some",
+      content: "foo foo foo foo",
+      author: "Emma Goldman"
+    };
+    const expectedKeys = ["id", "publishDate"].concat(Object.keys(newPost));
 
-      const expectedKeys = ["id", "publishDate"].concat(Object.keys(newPost));
+    return chai
+      .request(app)
+      .post("/blog-posts")
+      .send(newPost)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.all.keys(expectedKeys);
+        expect(res.body.title).to.equal(newPost.title);
+        expect(res.body.content).to.equal(newPost.content);
+        expect(res.body.author).to.equal(newPost.author);
+      });
+  });
 
-      return chai
-        .request(app)
-        .post("/blog-posts")
-        .send(newPost)
-        .then(function(res) {
-          expect(res).to.have.status(201);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a("object");
-          expect(res.body).to.have.all.keys(expectedKeys);
-          expect(res.body.title).to.equal(newPost.title);
-          expect(res.body.content).to.equal(newPost.content);
-          expect(res.body.author).to.equal(newPost.author);
-        });
-    });
+  it("should error if POST missing expected values", function() {
+    const badRequestData = {};
+    return chai
+      .request(app)
+      .post("/blog-posts")
+      .send(badRequestData)
+      .then(function(res) {
+        expect(res).to.have.status(400);
+      });
+  });
 
   it("should update blog posts on PUT", function() {
     return (
