d3df7164641ac6ffd6a8aad240565863eef9eb64
Scott Reyell <smreyell@gmail.com>
2018-07-22 16:30:08 -0700

    another try

--- blog-posts.js
+++ blog-posts.js
@@ -5,10 +5,21 @@ const {BlogPosts} = require('./models');
 
 // const BlogPost = require('./models').BlogPosts;
 
+function lorem() {
+  return (
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
+    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+    "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+    "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+    "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non " +
+    "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  );
+}
+
 BlogPosts.create(
-  'My First Blog Post','This is the content of my first blog post','John Bobin');
+  'My First Blog Post',lorem(),'John Bobin');
 BlogPosts.create(
-  'Monkey Chickens','Second blog post talking about monkey chickens','Jon John');
+  'Monkey Chickens',lorem(),'Jon John');
 
 // send back JSON representation of all recipes
 // on GET requests to root
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const BlogPostsRouter = require('./blog-posts');
-// log the http layer
+
 app.use(morgan('common'));
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
 
 
 // when requests come into `/blog-posts`
